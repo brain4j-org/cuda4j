363f853255bab279f41e2d5d2386885fca7d6a61
echo <xreflection@outlook.it>
2025-10-09 22:57:41 +0200

    feat(object): added default sizes for data types

--- cuda4j-java/src/main/java/org/cuda4j/CudaObject.java
+++ cuda4j-java/src/main/java/org/cuda4j/CudaObject.java
@@ -5,6 +5,13 @@ import java.lang.invoke.MethodHandle;
 
 public interface CudaObject {
     
+    long LONG_SIZE = Long.SIZE / 8; // 8
+    long DOUBLE_SIZE = Double.SIZE / 8; // 8
+    long INT_SIZE = Integer.SIZE / 8; // 4
+    long FLOAT_SIZE = Float.SIZE / 8; // 4
+    long SHORT_SIZE = Short.SIZE / 8; // 2
+    long CHAR_SIZE = Character.SIZE / 8;
+    long BYTE_SIZE = Byte.SIZE / 8; // 1
     Linker LINKER = Cuda4J.LINKER;
     SymbolLookup LOOKUP = Cuda4J.LOOKUP;
     MethodHandle CUDA_RELEASE_OBJECT = LINKER.downcallHandle(
